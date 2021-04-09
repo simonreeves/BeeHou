ad12f0631c68f072458c08c4212caa55066e7d83
Simon Reeves <simon@analogstudio.co.uk>
2021-04-09 19:49:35 +0100

    made script but need to be able to run in the background

--- scripts/python/bee/rs_tex_convert.py
+++ scripts/python/bee/rs_tex_convert.py
@@ -1,6 +1,6 @@
+import subprocess
 import os
 import hou
-
 
 
 def main(kwargs):
@@ -14,6 +14,30 @@ def main(kwargs):
     parm = parms[0]
 
     print('parm.eval(): {}'.format(parm.eval()))
+
+    # get dir
+    texfiles_dir = os.path.dirname(parm.eval())
+    print('texfiles_dir: {}'.format(texfiles_dir))
+    
+    # check if path exists
+    if not os.path.exists(texfiles_dir):
+        print('could not find folder')
+        return False
+
+    # get redshift path from environ variables
+    rs_tex_processor_path = hou.getenv('REDSHIFT_COREDATAPATH') + '/bin/redshiftTextureProcessor.exe'
+
+    print('rs_tex_processor_path: {}'.format(rs_tex_processor_path))
+
+    if not os.path.exists(rs_tex_processor_path):
+        print('could not find rstex exe')
+        return False
+
+    args = [rs_tex_processor_path, texfiles_dir + '/*']
+    
+    # start subprocess
+    subprocess.call(args)
+
 
 
 
