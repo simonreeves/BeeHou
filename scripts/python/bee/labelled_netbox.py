4afa8337f54f87b1bc4822c9c081bb893fb00d0a
Simon Reeves <simon@analogstudio.co.uk>
2023-01-13 17:37:24 +0000

    tidied labelled network box

--- scripts/python/bee/labelled_netbox.py
+++ scripts/python/bee/labelled_netbox.py
@@ -3,16 +3,21 @@ import hou
 
 def main():
     # find parent node of current node.
-    selected_nodes = hou.selectedNodes()  # type: tuple[hou.Node]
-    current_nodes = [x for x in selected_nodes if x.isCurrent]  # type: List[hou.Node]
+    selected_nodes = hou.selectedNodes()
+    current_nodes = [x for x in selected_nodes if x.isCurrent]
+
     if len(current_nodes) == 0:
-        print("there are no selected node. please select some nodes")
+        message = "There are no selected nodes, please select some nodes first"
+        hou.ui.setStatusMessage(message=message, severity=hou.severityType.ImportantMessage)
         return
-    parent = current_nodes[0].parent()  # type: hou.Node
+    
+    # Ask user for label, first looks tidier in network viewer
+    label = hou.ui.readInput('Label')
+
+    parent = current_nodes[0].parent()
 
     # create network boxes
-    # parent_network_box = parent.createNetworkBox()  # type: hou.NetworkBox
-    child_network_box = parent.createNetworkBox()  # type: hou.NetworkBox
+    child_network_box = parent.createNetworkBox()
 
     # find good position of boxes, which is center of selected nodes.
     list_x_of_selected_nodes = [node.position().x() for node in selected_nodes]
@@ -27,7 +32,7 @@ def main():
         child_network_box.fitAroundContents()
 
     # set color of network boxes
-    network_box_color = hou.Color(0.5,0.1,0.1)
+    network_box_color = hou.Color(0.5, 0.1, 0.1)
     child_network_box.setColor(network_box_color)
 
     # create sticky_note
@@ -38,25 +43,18 @@ def main():
     sticky_note_position = hou.Vector2(sticky_note_position_x, sticky_note_position_y)
     sticky_note.setPosition(sticky_note_position)
     
-    if hou.isUIAvailable:
-        label = hou.ui.readInput('Label')
-    else:
-        label = 'default'
-
     sticky_note.setText(label[1])
     sticky_note_height = 2
     sticky_note.setTextSize(sticky_note_height * .5)
+
     # min width to fit sticky note text
     sticky_note_width = max(child_network_box.size().x(), 4)
-
     sticky_note.setSize(hou.Vector2(sticky_note_width, sticky_note_height))
 
     # add after setting position
     child_network_box.addItem(sticky_note)
     
-    sticky_note_text_color = hou.Color(214.0 / 255.0, 214.0 / 255.0, 214.0 / 255.0)
     sticky_note.setDrawBackground(False)
-    sticky_note.setTextColor(sticky_note_text_color)
 
     return
 
