43fc0b40e429b3a06db3cce7db4b46abafc3b175
Simon Reeves <simon@analogstudio.co.uk>
2023-01-13 17:38:04 +0000

    focus null better ux (needs placement!)

--- scripts/python/bee/focus_null.py
+++ scripts/python/bee/focus_null.py
@@ -2,10 +2,15 @@ import hou
 
 def main():
     nodes = hou.selectedNodes()
+    # Init to record if a camera was selected at all
+    any_camera_selected = False
 
     for node in nodes:
         if node.type().name() != 'cam':
             continue
+        
+        # A camera was found!
+        any_camera_selected = True
 
         # create null
         focus_null = node.parent().createNode('null', node.name() + '_focus')
@@ -16,4 +21,9 @@ def main():
         focus_null.parm('shadedmode').set(1)
         focus_null.parm('controltype').set(2)
         focus_null.parm('geoscale').set(0.1)
-        focus_null.parmTuple('dcolor').set((0,0.1,1))
+        focus_null.parmTuple('dcolor').set((0, 0.1 ,1))
+
+    if not any_camera_selected:
+        message = "There were no cameras found in selection! Please select camera(s)"
+        hou.ui.setStatusMessage(message=message, severity=hou.severityType.ImportantMessage)
+        return
